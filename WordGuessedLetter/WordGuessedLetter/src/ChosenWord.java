52a140595eeaec5ae12b0a664ad1e4c3b7047de8
bdairshadi <bdairshadi@gmail.com>
2023-10-22 22:39:28 +0300

    v2 Added some description above important lines

--- WordGuessedLetter/WordGuessedLetter/src/ChosenWord.java
+++ WordGuessedLetter/WordGuessedLetter/src/ChosenWord.java
@@ -1,15 +1,24 @@
+// Import required classes for array handling
 import java.util.Arrays;
 
 public class ChosenWord {
+    // The actual word that the player needs to guess
     private final String word;
+
+    // An array to represent the letters of the word that have been revealed so far
     private char[] revealedLetters;
 
+    // Constructor initializes the word and the revealedLetters array
     public ChosenWord(String word) {
         this.word = word;
+
+        // Initialize the revealedLetters array with underscores
         this.revealedLetters = new char[word.length()];
         Arrays.fill(this.revealedLetters, '_');
     }
 
+    // This method checks if the guessed letter is in the word
+    // and updates the revealedLetters array accordingly
     public boolean guessLetter(char letter) {
         boolean isLetterInWord = false;
         for (int i = 0; i < word.length(); i++) {
@@ -21,6 +30,7 @@ public class ChosenWord {
         return isLetterInWord;
     }
 
+    // This method checks if the entire word has been guessed
     public boolean isFullyGuessed() {
         for (char c : revealedLetters) {
             if (c == '_') {
@@ -30,6 +40,7 @@ public class ChosenWord {
         return true;
     }
 
+    // Convert the revealedLetters array to a string for display
     @Override
     public String toString() {
         return new String(revealedLetters);
--- WordGuessedLetter/WordGuessedLetter/src/WordBank.java
+++ WordGuessedLetter/WordGuessedLetter/src/WordBank.java
@@ -1,12 +1,18 @@
+// Import required classes for list handling and random number generation
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class WordBank {
+    // A static list of words from which a random word will be chosen for the game
     private static final List<String> WORDS = Arrays.asList("programming", "java", "eclipse", "object", "inheritance");
 
+    // Method to fetch a random word from the WORDS list
     public static String getRandomWord() {
+        // Create a new Random object
         Random rand = new Random();
+
+        // Return a random word from the WORDS list
         return WORDS.get(rand.nextInt(WORDS.size()));
     }
 }
